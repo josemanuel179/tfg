b679425df53333b2af809c7fbf24d539e78aabca
José Manuel Martínez <josemanuel@MacBook-Pro-de-Jose.local>
2022-04-12 12:32:55 +0200

    Modificación Services

--- app/services.py
+++ app/services.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
+import os
 import nmap3
 nmap = nmap3.Nmap()
 
+ip = "127.0.0.1"
+
+package_manager = {
+    "REHL": ,
+    "Unbuntu": ,
+    "Debian": ,
+    "Arch": ,
+    "Open Suse": ,
+    "Suse": ,
+    "Oracle": ,
+    "Fedora": ,
+}
+
 def get_services():
     result = list()
+    services = nmap.nmap_version_detection(ip)[ip]["ports"]
 
-    services = nmap.nmap_version_detection("127.0.0.1")
-    print(services)
     for service in services:
-        print(service)
         result.append({
             "servicio": service["service"]["name"],
             "producto": service["service"]["product"],
@@ -17,10 +29,45 @@ def get_services():
     
     return result
 
-def main():
-    result = get_services()
-    print(result)
+def get_os():
+    os_data = os.popen('cat /etc/os-release').read().strip()
+    clean_os_data = os_data.split("\n")
+
+    for element in clean_os_data:
+        i = element.index("=")
+        keys.append(element[0:i])
+        values.append(element[i+2:-1])
+
+    os = dict(zip(keys, values))
+
+    return {
+        "os": {
+            "name": os["ID"]
+            "version": int(os["VERSION"])
+            "like": os["ID_LIKE"]
+        }
+    }
+
+def get_package_manager(os, os_like):
+    result = str()
+
+    if os in package_manager.keys():
+        result = package_manager[os]
+    elif os_like in package_manager.keys():
+        result = package_manager[os]
+    
+    return result
 
 
-if __name__ == '__main__':
-    main()
+def get_data():
+    os = get_os()
+    package_manager = get_package_manager()
+    services = get_services()
+    result = {**os, **package_manager, **services}
+    return result
+
+
+def update_data()
+    pass
+
+print(get_data)
