1812b34984f28c70043ae466c4a68c0d77c8c699
José Manuel Martínez <josemanuel@MacBook-Pro-de-Jose.local>
2022-09-05 00:53:57 +0200

    problemas 7

--- app/service.py
+++ app/service.py
@@ -342,6 +342,7 @@ def execute_analisys(ip, user, password, key='null'):
         # Almacenamiento de los datos en un fichero csv
         with open('/hermesd/hermes.csv', 'a') as f:
             writer = csv.writer(f)
+            writer.writerow("\n")
             writer.writerow([date.strftime("%Y-%m-%d %H:%M:%S"), commands[0].capitalize(), actual_services_len, update_versions_len, actual_services_len-last_versions_len, last_versions_len])
 
     # En caso contrario
